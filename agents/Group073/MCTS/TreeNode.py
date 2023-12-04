1410e851a1eb5debae26fdaae5bb5d9b3d1e1a8e
Ray7788 <ray778@foxmail.com>
2023-12-04 22:30:15 +0000

    Update attributes of the tree node

--- agents/Group073/MCTS/TreeNode.py
+++ agents/Group073/MCTS/TreeNode.py
@@ -1,15 +1,27 @@
-class TreeNode():
-    def __init__(self, state, parent):
+from Board import Board
+from Colour import Colour
+from Board import Board  
+from Move import Move
+
+class TreeNode:
+    def __init__(self, state: Board, parent, action: Move, colour: Colour):
         self.state = state
         self.parent = parent
+        self.action = action
+        self.colour = colour
+
         self.children = {}
         self.isTerminal = state.isTerminal()
         self.isFullyExpanded = self.isTerminal  # if the node is terminal, no need to expand
         self.numVisits = 0              
         self.totalReward = 0.0
-        
 
+    def get_child(self, action: Move):
+        """Get the child node of the current node by the action."""
+        return self.children[action]
+        
     def __str__(self):
+        """Returns a string representation of the node."""
         s=[]
         s.append("totalReward: %s"%(self.totalReward))
         s.append("numVisits: %d"%(self.numVisits))
