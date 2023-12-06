bbf77d802bc8057bd1b6e492c74b2f3d403f348c
Ray7788 <87214670+Ray7788@users.noreply.github.com>
2023-12-06 15:49:49 +0000

    Delete isTerminal and isFullyEXpanded

--- agents/Group073/MCTS/TreeNode.py
+++ agents/Group073/MCTS/TreeNode.py
@@ -14,19 +14,16 @@ class TreeNode:
         self.colour = colour
 
         self.children = {}  # a map from action to TreeNode
-        self.isTerminal = state.isTerminal()
-        # TODO: simplify isTerminal and isFullyExpanded
-        self.isFullyExpanded = self.isTerminal  # if the no1de is terminal, no need to expand
-        self.numVisits = 0
-        self.totalReward = 0.0
+        self.numVisits = 0      # N(s, a)   the number of times that action a has been taken from state s
+        self.totalReward = 0.0      # Q(s, a)   the total reward of all the simulations that started from state s and took action a
 
     def get_child(self, action: Move):
         """Get the child node of the current node by the action."""
         return self.children[action]
 
-    def get_children(self) -> List[Node]:
+    def get_children(self) -> {}:
         """Get all the children of the current node."""
-        return self.children.values()
+        return self.children.items()
 
     def __str__(self):
         """Returns a string representation of the node."""
