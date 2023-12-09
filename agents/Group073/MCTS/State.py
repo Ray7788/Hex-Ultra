c4086b0d62a3c14ca6f4e4cbf6738429406b6712
Ray7788 <87214670+Ray7788@users.noreply.github.com>
2023-12-09 01:50:58 +0000

    Update Node part

--- agents/Group073/MCTS/State.py
+++ agents/Group073/MCTS/State.py
@@ -3,14 +3,16 @@ from typing import List
 from Board import Board
 from Colour import Colour
 from Move import Move
+from random import choice
+import random
 
 
-class State:
-    def __init__(self, board: Board, current_player=1):
-        self.board = board
+class State(Board):
+    def __init__(self, current_player=1):
+        super().__init__()
         self.current_player = current_player  # 1 for blue, -1 for red，default is blue
-        self.is_terminal = self.is_terminal()
-        self.current_value = self.getReward()
+        # self.is_terminal = self.is_terminal()
+        self.current_value = self.get_reward()
         self.current_round_index = 0
         self.cumulative_choices = []
 
@@ -21,31 +23,43 @@ class State:
         """
         Returns the list of all possible moves based on the current state(if it is coloured) of each tile.
         """
-        choices = List[Move] = []  # Stores valid moves
+        all_moves: List[Move] = []  # Stores all moves
+        choices: List[Move] = []  # Stores valid moves
 
-        for i in range(self.board.get_size()):
-            for j in range(self.board.get_size()):
-                tiles = self.board.get_tiles()
-                if tiles[i][j].colour is None:
-                    choices.append(Move(colour=colour, x=i, y=j))
+        for i in range(self.get_size()):
+            for j in range(self.get_size()):
+                all_moves.append(Move(colour=colour, x=i, y=j))
+
+        for m in all_moves:
+            # tiles = self.board.get_tiles()
+            # if tiles[i][j].colour is None:
+            #     choices.append(Move(colour=colour, x=i, y=j))
+            tile = self.get_tiles()[m.x][m.y]
+            if tile.get_colour() is None:
+                choices.append(m)
 
         return choices
 
-    def takeAction(self, action):
-        playerColour = Colour.BLUE if self.current_player == 1 else Colour.RED
-        new_state = deepcopy(self)
-        new_state.board._tiles[action[0]][action[1]].colour = playerColour
-        new_state.current_player = -self.current_player
-        return new_state
+    def take_action(self, action):
+        # player_colour = Colour.BLUE if self.current_player == 1 else Colour.RED
+        new_state = deepcopy(action)
+        action.move(new_state)
+        v = TreeNode(new_state, node, action, node.colour.opposite())
+        # new_state.board._tiles[action[0]][action[1]].colour = player_colour
+        # new_state.current_player = -self.current_player
+        return action
 
     def is_terminal(self):
-        return self.board.has_ended()
+        return self.has_ended()
 
-    def getReward(self):
+    def get_reward(self):
+        """
+        Returns the reward for the current state.
+        """
         if not self.is_terminal():
             return None  # or raise an error, since reward is only for terminal states
 
-        winner = self.board.get_winner()  # Hypothetical method to determine the winner
+        winner = self.get_winner()  # Hypothetical method to determine the winner
         if winner is None:
             return 0  # Draw case, if applicable
         return 1 if winner == self.current_player else -1
--- agents/Group073/MCTS/TreeNode.py
+++ agents/Group073/MCTS/TreeNode.py
@@ -4,6 +4,7 @@ from Board import Board
 from Colour import Colour
 from Board import Board
 from Move import Move
+from State import State
 
 
 class TreeNode:
@@ -13,23 +14,46 @@ class TreeNode:
         self.action = action
         self.colour = colour
 
-        self.children = {}  # a map from action to TreeNode
-        self.numVisits = 0      # N(s, a)   the number of times that action a has been taken from state s
-        self.totalReward = 0.0      # Q(s, a)   the total reward of all the simulations that started from state s and took action a
+        self.children: List[TreeNode] = []  # a list which saves all children TreeNode
+        self.numVisits = 0      # N(s, a) the number of times that action a has been taken from state s
+        self.totalReward = 0.0      # Q(s, a) the total reward of all the simulations that started from state s and took action a
 
-    def get_child(self, action: Move):
-        """Get the child node of the current node by the action."""
-        return self.children[action]
+        # self.is_terminal = state.is_terminal()
+        # self.isFullyExpanded = self.is_terminal
 
-    def get_children(self) -> {}:
+    def get_children(self):
         """Get all the children of the current node."""
-        return self.children.items()
+        return self.children
+
+    def get_possible_actions(self, colour: Colour, board_size: int) -> List[Move]:
+        """
+        Returns the list of all possible moves based on the current state(if it is coloured) of each tile.
+        """
+        all_moves: List[Move] = []  # Stores all moves
+        choices: List[Move] = []  # Stores valid moves
+
+        for i in range(board_size):
+            for j in range(board_size):
+                all_moves.append(Move(colour=colour, x=i, y=j))
+                # tiles = self.state.get_tiles()
+                # if tiles[i][j].colour is None:
+                #     choices.append(Move(colour=colour, x=i, y=j))
+
+        for m in all_moves:
+            tile = self.state.get_tiles()[m.x][m.y]
+            if tile.get_colour() is None:
+                choices.append(m)
+
+        return choices
 
     def __str__(self):
-        """Returns a string representation of the node."""
+        """
+        Returns a string representation of the node.
+        TEST ONLY!!
+        """
         s = []
         s.append("totalReward: %s" % (self.totalReward))
         s.append("numVisits: %d" % (self.numVisits))
-        s.append("isTerminal: %s" % (self.isTerminal))
-        s.append("possibleActions: %s" % (self.children.keys()))
+        s.append("isTerminal: %s" % (self.is_terminal))
+        s.append("possibleActions: %s" % (self.children))
         return "%s: {%s}" % (self.__class__.__name__, ', '.join(s))
