7b0d58b9023e51366d91cf37f95ba562b6d49cb2
Ray7788 <87214670+Ray7788@users.noreply.github.com>
2023-12-09 01:50:27 +0000

    make the agent function calling part simple
    Have a bug: Illegal move appears in the end of the game

--- agents/Group073/MCTS/MCTSAgent.py
+++ agents/Group073/MCTS/MCTSAgent.py
@@ -4,11 +4,17 @@ from BasicAgent import NaiveAgent
 from State import State
 from mcts import mcts
 from Board import Board
+from Colour import Colour
 
 class MCTSAgent(NaiveAgent):
     """
     This class describes the Hex agent using MCTS strategy.
     """
+
+    def __init__(self, board_size=11):
+        super().__init__(board_size)
+        self.mct = mcts(timeLimit=1000, iterationLimit=None, colour=self.colour, board_size=self.board_size)
+
     def make_move(self):
         if self.colour == "B" and self.turn_count == 0:
             # random choose if swap, should be changed later
@@ -25,20 +31,21 @@ class MCTSAgent(NaiveAgent):
                 self.s.sendall(bytes(f"{pos[0]},{pos[1]}\n", "utf-8"))
                 self.board[pos[0]][pos[1]] = self.colour
         else:
+            # ---------------------------------------------------------------
             # print(self.board)
             converted_string = str(','.join(''.join(str(item) for item in row) for row in self.board))
             # print("converted_string---->>>", converted_string)
-            b = Board.from_string(string_input=converted_string)
-            myColour = 1 if self.colour == "B" else -1
-            current_state = State(board=b, current_player=myColour)
+
+            # myColour = 1 if self.colour == "B" else -1
+            # current_state = State(current_player=myColour)
             # print("current_state---->>>", current_state.board.print_board())
-            searcher = mcts(timeLimit=1000)
-            action = searcher.search(initialState=current_state)
-            # print("go", action)
-            self.s.sendall(bytes(f"{action[0]},{action[1]}\n", "utf-8"))
+            action = mcts(timeLimit=1000, colour=Colour.from_char(self.colour)).search(initial_state=converted_string)
+            # ---------------------------------------------------------------
+            self.s.sendall(bytes(f"{action[0]},{action[1]}\n", "utf-8"))    # coordinate (x, y)
             self.board[action[0]][action[1]] = self.colour
         self.turn_count += 1
-        
+
+
 if (__name__ == "__main__"):
     agent = MCTSAgent()
     agent.run()
